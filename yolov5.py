795f4961a814ef5ae6020b903bc45bd6a385fcc1
iaiamomo <flavia.monti95@gmail.com>
2024-06-22 19:13:47 +0200

    update scripts

--- yolov5.py
+++ yolov5.py
@@ -4,19 +4,31 @@ from clearml import Task
 import dotenv
 dotenv.load_dotenv()
 
-models = ['n', 's', 'm']
-datasets = ['dataset_f', 'dataset_h']
-idx = 0
+cwd = os.getcwd()
+
+with open(f'{cwd}/outv5.txt', 'w') as f:
+    print('YOLOv5 training', file=f)
+
+batches = {
+    'n': 64,
+    's': 64,
+    'm': 32
+}
+models = list(batches.keys())
+datasets = ['dataset_fold', 'dataset_fold_negative', 'dataset_hole', 'dataset_hole_negative']
+idx = 1
 for dataset in datasets:
-    shutil.copy(f'/home/calcio/cardboard_yolo/{dataset}/data.yaml', '/home/calcio/cardboard_yolo/yolov5/data')
+    shutil.copy(f'{cwd}/{dataset}/data.yaml', f'{cwd}/yolov5/data')
     for dim in models:
-        os.chdir('/home/calcio/cardboard_yolo/yolov5')
-        task = Task.init(project_name='yolo', task_name=dim, tags=[dim, dataset, 'yolov5'])
-        res = os.system("python3 train.py --img 640 --batch 32 --epochs 400 --patience 50 --optimizer 'SGD' --data data/data.yaml --weights yolov5m.pt")
-        task.close()
-        if idx == 0:
+        if idx == 1:
             run_res = "exp"
         else: run_res = f"exp{idx}"
-        print(f"{dataset} - {dim} - {run_res}")
+        with open(f'{cwd}/outv5.txt', 'a') as f:
+            print(f"{dataset} - YOLOv5{dim} - {run_res}", file=f)
         idx+=1
-        res = os.system(f"python3 detect.py --weights runs/train/{run_res}/weights/best.pt --img 640 --source ../{dataset}/test/images --line-thickness 1 --save-txt")
+        
+        os.chdir(f'{cwd}/yolov5')
+        task = Task.init(project_name='yolo', task_name=f"{dataset} - {dim} - {run_res}", tags=[dim, dataset, 'yolov5'])
+        res = os.system(f"python3 train.py --img 640 --batch {batches[dim]} --epochs 500 --patience 50 --optimizer 'SGD' --data data/data.yaml --weights yolov5{dim}.pt")
+        task.close()
+        #res = os.system(f"python3 detect.py --weights runs/train/{run_res}/weights/best.pt --img 640 --source ../{dataset}/test/images --line-thickness 1 --save-txt")
--- yolov8.py
+++ yolov8.py
@@ -1,23 +1,42 @@
 import os
 from ultralytics import YOLO
 from clearml import Task
+import gc
+import torch
 import dotenv
+import time
 dotenv.load_dotenv()
 
-models = ['n', 's', 'm']
-datasets = ['dataset_f', 'dataset_h']
+cwd = os.getcwd()
+
+with open(f'{cwd}/outv8.txt', 'w') as f:
+    print('YOLOv8 training', file=f)
+
+batches = {
+    'n': 32,
+    's': 32,
+    'm': 16
+}
+models = list(batches.keys())
+datasets = ['dataset_fold' 'dataset_fold_negative', 'dataset_hole', 'dataset_hole_negative']
 idx = 0
 for dataset in datasets:
     for dim in models:
-        os.chdir('/home/calcio/cardboard_yolo/yolov8')
-        task = Task.init(project_name='yolo', task_name=dim, tags=[dim, dataset, 'yolov8'])
-        model = YOLO(f'yolov8{dim}.pt')
-        model.train(data=f"/home/calcio/cardboard_yolo/{dataset}/data.yaml", epochs=400, batch=32, imgsz=640, patience=50)
-        task.close()
-        if idx == 0:
+        if idx == 1:
             run_res = "train"
         else: run_res = f"train{idx}"
-        print(f"{dataset} - {dim} - {run_res}")
+        with open(f'{cwd}/outv8.txt', 'a') as f:
+            print(f"{dataset} - YOLOv8{dim} - {run_res}", file=f)
         idx+=1
-        model = YOLO(f"/home/calcio/cardboard_yolo/yolov8/runs/detect/{run_res}/weights/best.pt")
-        results = model.predict(f"/home/calcio/cardboard_yolo/{dataset}/test/images/", save=True, imgsz=640, conf=0.7)
+
+        os.chdir(f'{cwd}/yolov8')
+        task = Task.init(project_name='yolo', task_name=f"{dataset} - {dim} - {run_res}", tags=[dim, dataset, 'yolov8'])
+        model = YOLO(f'yolov8{dim}.pt')
+        model.train(data=f"{cwd}/{dataset}/data.yaml", epochs=500, batch=batches[dim], imgsz=640, patience=50, optimizer='SGD')
+        task.close()
+        #model = YOLO(f"{cwd}/yolov8/runs/detect/{run_res}/weights/best.pt")
+        #results = model.predict(f"{cwd}/{dataset}/test/images/", save=True, imgsz=640, conf=0.7)
+        
+        gc.collect()
+        torch.cuda.empty_cache()
+        time.sleep(60)
